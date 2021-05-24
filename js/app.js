c7f0411cee4755d31e4d3b5b840c95edfcd29220
Tamara <tami.pg94@gmail.com>
2021-05-24 18:45:39 +0200

    examen terminado

--- js/app.js
+++ js/app.js
@@ -3,16 +3,21 @@ function CalcularMedia() {
     var bbdd
     var eedd
     var llmm
-    var media;
+    var ing
+    var media
+    var media2;
 
     prog = document.getElementById("programacion").value;
     bbdd = document.getElementById("bbdd").value;
     eedd = document.getElementById("eedd").value;
     llmm = document.getElementById("llmm").value;
+    ing = document.getElementById("ing").value;
 
     media = (parseFloat(prog) + parseFloat(bbdd) + parseFloat(eedd) + parseFloat(llmm)) / 4;
+    media2= (parseFloat(prog) + parseFloat(bbdd) + parseFloat(eedd) + parseFloat(llmm) + parseFloat(ing)) / 5;
 
     document.getElementById("media").value = media.toFixed(2);
+    document.getElementById("media2").value = media2.toFixed(2);
 
 
     return false;
